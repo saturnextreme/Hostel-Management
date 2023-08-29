0c15cd4f988e4e7fd5f8ddc95f496d491e49f401
Abineth Anantharam <98078409+abinetha@users.noreply.github.com>
2023-08-29 22:31:47 +0530

    Update models.py

--- home/models.py
+++ home/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Hostel(models.Model):
-    name = models.CharField(max_length=100)
+    roll_no = models.ForeignKey('Student', on_delete=models.CASCADE)
     floor_no = models.CharField(max_length=10)
-    room_no = models.CharField(max_length=10)
+    room_no = models.ForeignKey('Room', on_delete=models.CASCADE)
     vacancy = models.BooleanField(default=True)
     date_of_join = models.DateField()
     validity = models.DateField()
@@ -17,10 +17,10 @@ class Room(models.Model):
     chair = models.BooleanField(default=True)
     bed = models.BooleanField(default=True)
     cleaning = models.BooleanField(default=True)
-    hostel = models.ForeignKey(Hostel, on_delete=models.CASCADE)
+    roll_no = models.ForeignKey('Student', on_delete=models.CASCADE)
 
 class Academics(models.Model):
-    roll_no = models.CharField(primary_key=True, max_length=10)
+    roll_no = models.ForeignKey('Student', on_delete=models.CASCADE)
     department = models.CharField(max_length=50)
     course = models.CharField(max_length=50)
     year = models.IntegerField()
@@ -28,21 +28,20 @@ class Academics(models.Model):
 
 class Student(models.Model):
     roll_no = models.CharField(primary_key=True, max_length=10)
+    image = models.ImageField(upload_to=None)
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
     address = models.TextField()
     date_of_birth = models.DateField()
     guardian = models.CharField(max_length=100)
-    guardian_mobile_nu = models.CharField(max_length=15)
+    guardian_mobile_no = models.CharField(max_length=15)
     blood_group = models.CharField(max_length=5)
     age = models.IntegerField()
     email = models.EmailField()
-    room_no = models.ForeignKey(Room, on_delete=models.CASCADE)
-    academics = models.ForeignKey(Academics, on_delete=models.CASCADE)
-    entry_exit_form = models.ForeignKey('EntryExitForm', on_delete=models.CASCADE)
+    room_no = models.ForeignKey('Room', on_delete=models.CASCADE)
 
 class EntryExitForm(models.Model):
-    roll_no = models.ForeignKey(Student, on_delete=models.CASCADE)
+    roll_no = models.ForeignKey('Student', on_delete=models.CASCADE)
     entry_date = models.DateField()
     exit_date = models.DateField()
     entry_time = models.TimeField()
@@ -51,17 +50,12 @@ class EntryExitForm(models.Model):
     reason_for_leaving = models.TextField()
 
 class Complaint(models.Model):
-    roll_no = models.ForeignKey(Student, on_delete=models.CASCADE)
+    roll_no = models.ForeignKey('Student', on_delete=models.CASCADE)
     complaint = models.TextField()
     is_addressed = models.BooleanField(default=False)
-    resolved_by = models.ForeignKey('Admin', on_delete=models.CASCADE, null=True, blank=True)
 
 class Admin(models.Model):
     username = models.CharField(primary_key=True, max_length=50)
     password = models.CharField(max_length=100)
     notification = models.TextField()
 
-class Notification(models.Model):
-    text = models.TextField()
-    hostel = models.ForeignKey(Hostel, on_delete=models.CASCADE)
-    admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
